a1e16f863e0cc147f3fd37a2520659926b1e8158
pmerritt-cmu <pmerritt@andrew.cmu.edu>
2026-01-11 15:16:58 -0500

    renaming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from cmu_graphics import *
-from app_init import onAppStart
+from init import onAppStart
 from drawing import redrawAll
 from classes import *
 from handlers import onMouseMove, onMouseDrag, onMousePress, onMouseRelease, onKeyPress, onKeyHold, onStep
